cafc5d836e7c9356a769e0dfe206a1303df19110
cjosue13 <solomaj12@gmail.com>
2019-10-03 23:54:40 -0600

    secuencia

--- WsClinicaUNA/WsClinicaUNA/src/java/cr/ac/una/wsclinicauna/model/Antecedente.java
+++ WsClinicaUNA/WsClinicaUNA/src/java/cr/ac/una/wsclinicauna/model/Antecedente.java
@@ -10,11 +10,14 @@ import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -36,7 +39,8 @@ public class Antecedente implements Serializable {
     private static final long serialVersionUID = 1L;
     // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
     @Id
-
+    @SequenceGenerator(name = "ANT_ID_GENERATOR", sequenceName = "ClinicaUNA.SEQ_ANTECEDENTES", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ANT_ID_GENERATOR")
     @Basic(optional = false)
     @Column(name = "ANT_ID")
     private Long antId;
